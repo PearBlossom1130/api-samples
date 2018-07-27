eae1f8c629b75234ab6bd5774830c5f99abb2997
Hwasoo Lee <Hwasoo.Lee@versanthealth.com>
2018-07-27 10:46:34 -0400

    [CHANNEL] Add channel and PageToken

--- dotnet/Google.Apis.YouTube.Samples.Search/Search.cs
+++ dotnet/Google.Apis.YouTube.Samples.Search/Search.cs
@@ -42,70 +42,83 @@ namespace Google.Apis.YouTube.Samples
   /// </summary>
   internal class Search
   {
-    [STAThread]
-    static void Main(string[] args)
-    {
-      Console.WriteLine("YouTube Data API: Search");
-      Console.WriteLine("========================");
+	[STAThread]
+	static void Main(string[] args)
+	{
+	  Console.WriteLine("YouTube Data API: Search");
+	  Console.WriteLine("========================");
 
-      try
-      {
-        new Search().Run().Wait();
-      }
-      catch (AggregateException ex)
-      {
-        foreach (var e in ex.InnerExceptions)
-        {
-          Console.WriteLine("Error: " + e.Message);
-        }
-      }
+	  try
+	  {
+		new Search().Run().Wait();
+	  }
+	  catch (AggregateException ex)
+	  {
+		foreach (var e in ex.InnerExceptions)
+		{
+		  Console.WriteLine("Error: " + e.Message);
+		}
+	  }
 
-      Console.WriteLine("Press any key to continue...");
-      Console.ReadKey();
-    }
+	  Console.WriteLine("Press any key to continue...");
+	  Console.ReadKey();
+	}
 
-    private async Task Run()
-    {
-      var youtubeService = new YouTubeService(new BaseClientService.Initializer()
-      {
-        ApiKey = "REPLACE_ME",
-        ApplicationName = this.GetType().ToString()
-      });
+	private async Task Run()
+	{
+	  var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+	  {
+		ApiKey = "REPLACE_ME",
+		ApplicationName = this.GetType().ToString()
+	  });
 
-      var searchListRequest = youtubeService.Search.List("snippet");
-      searchListRequest.Q = "Google"; // Replace with your search term.
-      searchListRequest.MaxResults = 50;
+	  var searchListRequest = youtubeService.Search.List("snippet");
+	  searchListRequest.Q = "Google"; // Replace with your search term.
+									  // In order to get all video list for a channel we have to loop through using PageToken since
+									  // the max results is 50.
 
-      // Call the search.list method to retrieve results matching the specified query term.
-      var searchListResponse = await searchListRequest.ExecuteAsync();
 
-      List<string> videos = new List<string>();
-      List<string> channels = new List<string>();
-      List<string> playlists = new List<string>();
+		searchListRequest.MaxResults = 50;
 
-      // Add each result to the appropriate list, and then display the lists of
-      // matching videos, channels, and playlists.
-      foreach (var searchResult in searchListResponse.Items)
-      {
-        switch (searchResult.Id.Kind)
-        {
-          case "youtube#video":
-            videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
-            break;
+	  // Call the search.list method to retrieve results matching the specified query term.
+	  var searchListResponse = await searchListRequest.ExecuteAsync();
 
-          case "youtube#channel":
-            channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
-            break;
+		int count = 0;
+		while (searchListResponse.NextPageToken != null)
+		{
+			searchListRequest.PageToken = searchListResponse.NextPageToken;
+			searchListResponse = searchListRequest.Execute();
+			count++;
+		}
 
-          case "youtube#playlist":
-            playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
-            break;
-        }
-      }
 
-      Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
-      Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
-      Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
-    }
+		List<string> videos = new List<string>();
+	  List<string> channels = new List<string>();
+	  List<string> playlists = new List<string>();
+
+	  // Add each result to the appropriate list, and then display the lists of
+	  // matching videos, channels, and playlists.
+	  foreach (var searchResult in searchListResponse.Items)
+	  {
+		switch (searchResult.Id.Kind)
+		{
+		  case "youtube#video":
+			videos.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.VideoId));
+			break;
+
+		  case "youtube#channel":
+			channels.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.ChannelId));
+			break;
+
+		  case "youtube#playlist":
+			playlists.Add(String.Format("{0} ({1})", searchResult.Snippet.Title, searchResult.Id.PlaylistId));
+			break;
+		}
+	  }
+
+	  Console.WriteLine(String.Format("Videos:\n{0}\n", string.Join("\n", videos)));
+	  Console.WriteLine(String.Format("Channels:\n{0}\n", string.Join("\n", channels)));
+	  Console.WriteLine(String.Format("Playlists:\n{0}\n", string.Join("\n", playlists)));
+	}
   }
 }
